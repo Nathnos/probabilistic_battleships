98abdc4d07c6bcb2200aa90070ca18de7542c7ad
Lorenzo VILLARD <villard.lorenzo.pro@pm.me>
2020-10-15 17:16:08 +0200

    Linted

--- tests/boats_positions_test.py
+++ tests/boats_positions_test.py
@@ -9,9 +9,9 @@ class BoatPositionsTest(unittest.TestCase):
         for boat in range(1, 6):
             self.assertEqual(
                 combinatorial.boats_positions.number_of_positions([boat]),
-                combinatorial.single_boat_positions.number_of_positions(boat)
+                combinatorial.single_boat_positions.number_of_positions(boat),
             )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
